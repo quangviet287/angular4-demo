ab528514eed37a6680ad70cf888e41e7dcadd379
Quang Viet <quangviet287@gmail.com>
2018-10-10 23:52:51 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoAngular4';
+
+  months = ['January', 'Feburary', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September',
+    'October', 'November', 'December'];
+
+  isavailable = false;
+
+  myClickFunction(event) {
+    alert('Button is clicked');
+    console.log(event);
+  }
+
+  changemonth(event) {
+    alert('Month is changed');
+    console.log(event);
+    this.isavailable = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { ChangeTextDirective } from './change-text.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCmpComponent,
+    ChangeTextDirective
   ],
   imports: [
     BrowserModule
