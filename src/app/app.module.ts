25c26f69cd5eba78532ee989cec1cab82d637b46
Quang Viet <quangviet287@gmail.com>
2018-10-12 00:01:52 +0700

    add pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ChangeTextDirective } from './change-text.directive';
+import {SqrtPipe} from './app.sqrt';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewCmpComponent,
-    ChangeTextDirective
+    ChangeTextDirective,
+    SqrtPipe
   ],
   imports: [
     BrowserModule
